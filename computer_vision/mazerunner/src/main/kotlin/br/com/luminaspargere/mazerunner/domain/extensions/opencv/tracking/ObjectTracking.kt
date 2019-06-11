a1fd44cdeb9960162e35e2502c9e906fdd9fd3c6
Icaro Temponi <icarohs7@gmail.com>
2019-06-11 20:53:41 -0300

    auto commit push on 11/06/2019 20:53

--- computer_vision/mazerunner/src/main/kotlin/br/com/luminaspargere/mazerunner/domain/extensions/opencv/tracking/ObjectTracking.kt
+++ computer_vision/mazerunner/src/main/kotlin/br/com/luminaspargere/mazerunner/domain/extensions/opencv/tracking/ObjectTracking.kt
@@ -63,7 +63,7 @@ object ObjectTracking : CoroutineScope by GlobalScope {
         println("src2dst => $src2Dst")
         println("tip2dst => $tip2Dst")
         if (src2Dst > 85) {
-            if (tip2Dst > (src2Dst + 10)) arduinoControlRepository.turnRight()
+            if (tip2Dst > (src2Dst - 5)) arduinoControlRepository.turnRight()
             else arduinoControlRepository.goForward()
         } else {
             throw NullPointerException()
